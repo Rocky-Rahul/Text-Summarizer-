532ab59ed0b262374abecc285b4107cc9b4b1c16
Rocky-Rahul <rakeshrocky28230@gmail.com>
2024-10-14 18:47:31 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textsummarizer.logging import logger
 
 def execute_stage(stage_name, pipeline_class):
@@ -15,6 +16,6 @@ def execute_stage(stage_name, pipeline_class):
 # Usage
 execute_stage("Data Ingestion", DataIngestionTrainingPipeline)
 execute_stage("Data Validation", DataValidationTrainingPipeline)
-#execute_stage("Data Transformation", DataTransformationTrainingPipeline)
+execute_stage("Data Transformation", DataTransformationTrainingPipeline)
 #execute_stage("Model Trainer", ModelTrainerTrainingPipeline)
 #execute_stage("Model Evaluation", ModelEvaluationTrainingPipeline)
