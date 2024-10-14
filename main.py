5075098045fbb4d713159ecfce9a25913428f951
Rocky-Rahul <rakeshrocky28230@gmail.com>
2024-10-14 16:03:11 +0530

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+def execute_stage(stage_name, pipeline_class):
+    try:
+        logger.info(f">>>>>> stage {stage_name} started <<<<<<")
+        stage_pipeline = pipeline_class()
+        stage_pipeline.main()
+        logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+# Usage
+execute_stage("Data Ingestion", DataIngestionTrainingPipeline)
+#execute_stage("Data Validation", DataValidationTrainingPipeline)
+#execute_stage("Data Transformation", DataTransformationTrainingPipeline)
+#execute_stage("Model Trainer", ModelTrainerTrainingPipeline)
+#execute_stage("Model Evaluation", ModelEvaluationTrainingPipeline)
